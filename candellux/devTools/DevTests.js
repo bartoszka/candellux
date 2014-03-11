b26c4afe45becd97f869a4cf5997cede33ff95d6
bartoszka <bartosz.klewiado@gmail.com>
2014-03-11 18:54:59 +0100

    Update #5

--- candellux/devTools/DevTests.js
+++ candellux/devTools/DevTests.js
@@ -1,1 +1,1 @@
-﻿//userEnt = new ds.User({username: "test", password: "test"});//userEnt.save();//ds.User.all()[0].groups//ds.Person.all()//ds.User.remove();//ds.Person.remove();
+﻿//loginByPassword("desi", "mysza");//userEnt = new ds.User({username: "test", password: "test"});//userEnt.save();//ds.User.all()[0].groups//ds.Person.all()//ds.User.remove();//ds.Person.remove();
